a631450371648ba1b1499fce633a9e963aeadcae
Christophe HENRY <chenry@linagora.com>
2019-05-24 14:35:58 +0200

    Minimum reproductible test of sass-loader#683

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <b-link to="/">Home</b-link> |
+      <b-link to="/about">About</b-link>
     </div>
     <router-view/>
   </div>
